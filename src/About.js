85e078988edb55595fef3a85342d34dc663d88b7
PIPATPHARKUBON <you@example.com265345221017-st@rmutsb.ac.th>
2023-05-12 11:33:58 +0700

    first commit

---
+++ src/About.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export default function About() {
+  return (
+    <center>
+    <div className="col-sm-3">
+   <div class="card" >
+  <img src="./imges/cat.jpg" class="card-img-top" width="50%"/>
+  <div class="card-body">
+    <h5 class="card-title">Profile</h5>
+    <p class="card-text">เป็นคนดี</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+   </div>
+   </center>
+  )
+}
---
+++ src/product.js
@@ -0,0 +1,38 @@
+import React from 'react'
+
+export default function Product() {
+  return (
+    <div className="container mt-5">
+    <center>
+<h3 >PRODUCTS</h3>
+<hr></hr>
+</center>
+    <div className="row">
+        <div className="col-sm-4">
+            <img src='./imges/p1.jpg' width="250" />
+            <h3>Column 1</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit...</p>
+            <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris...</p>
+        </div>
+        <div className="col-sm-4">
+        <h3><img src='./imges/p1.jpg' width="250" /></h3>
+        <h3>Column 1</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit...</p>
+            <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris...</p>
+        </div>
+        <div className="col-sm-4">
+        <h3><img src='./imges/p1.jpg' width="250" /></h3>
+        <h3>Column 1</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit...</p>
+            <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris...</p>
+        </div>
+        <div className="col-sm-4">
+        <h3><img src='./imges/p1.jpg' width="250" /></h3>
+        <h3>Column 1</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit...</p>
+            <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris...</p>
+        </div>
+    </div>
+</div>
+  )
+}
